1220cd3abbce8b2dcd867a40d0401d2b03493594
Zach Thompson <zach@zachmakesgames.com>
2021-07-21 15:40:30 -0700

    Cleaning repo to make way for new iteration

--- ShaderPack/glm_includes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "vec2.hpp"
-#include "vec3.hpp"
-#include "vec4.hpp"
-#include "mat2x2.hpp"
-#include "mat3x3.hpp"
-#include "mat4x4.hpp"
--- Spock/Graphics/IRenderable.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class IRenderable
-{
-	public:
-		virtual void Update(float dt) = 0;
-		virtual void Init() = 0;
-};
-
