557fa85162772eb8493666364494fcffa1c43838
Zach Thompson <zach@zachmakesgames.com>
2021-08-27 11:29:14 -0700

    Implemented Instance and Device classes

--- SpockLib/Device.h
+++ SpockLib/Device.h
@@ -1,6 +1,34 @@
 #pragma once
 #include "Instance.h"
+#include <exception>
+#include <vector>
 
 class Device {
+	public:
+		Device(Instance* instance);
+		VkPhysicalDevice* GetPhysDevice();
+		VkDevice* GetLogicalDevice();
+		int FindMemory(VkMemoryPropertyFlagBits memoryFlagBits, uint32_t  memoryTypeBits);
+		VkCommandBuffer* GetGfxCmdBuffers(int* outCount);
+		VkCommandBuffer* GetTransferCmdBuffer();
 
+	private:
+		Instance* instance; 
+		VkPhysicalDevice physDevice;
+		VkDevice logicalDevice;
+		VkQueue queue;
+		VkCommandPool gfxCmdPool;
+		VkCommandPool transferCmdPool;
+		VkCommandBuffer* gfxCmdBuffers;
+		VkCommandBuffer* transferCmdBuffer;
+		VkDeviceQueueCreateInfo deviceQueueCreateInfo;
+		VkDeviceCreateInfo deviceCreateInfo;
+		VkCommandPoolCreateInfo gfxPoolCreateInfo;
+		VkCommandPoolCreateInfo transferCreateInfo;
+
+		VkCommandBufferAllocateInfo gfxBufferAllocInfo;
+		VkCommandBufferAllocateInfo transferBufferAllocInfo;
+		uint32_t graphicsBufferCount = 3;
+		uint32_t transferBufferCount = 1;
+		
 };
--- SpockLib/Instance.h
+++ SpockLib/Instance.h
@@ -1,9 +1,18 @@
 #pragma once
+#include <string>
 #include "vulkan/vulkan.h"
 
 //Using GLFW for simplicity right now, want to be able to use native windows eventually
 #include "GLFW/glfw3.h"
 
 class Instance {
+	public:
+		Instance(std::string instance_name);
+		VkInstance* GetInstance();
 
+	private:
+		std::string instanceName;
+		VkInstance instance;
+		VkApplicationInfo appInfo;
+		VkInstanceCreateInfo instCreateInfo;
 };
